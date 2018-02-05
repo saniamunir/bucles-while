1021650481231ed214e633751134a734a2245008
ism1900830 <ism1900830@a22.informatica.escoladeltreball.org>
2018-02-05 12:16:40 +0100

    while3

---
+++ bucles3.py
@@ -0,0 +1,8 @@
+# coding: utf
+1= int(input("Escriba un número: "))
+2= int(input("Escriba un número mayor que " + str(1) + ": "))
+while 2 > 1:
+    1 = 2
+    2 = int(input("Escriba un número mayor que " + str(1) + ": "))
+print(2, "no es mayor que", str(1) + ".")
+
