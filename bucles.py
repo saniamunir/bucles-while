e7f3d6c87ae975418d912de82fc0264f8e0f8265
ism1900830 <ism1900830@a22.informatica.escoladeltreball.org>
2018-02-05 12:10:13 +0100

    while1

---
+++ bucles.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+a = int(input("Dime un numero"))
+b = int(input("Dime un numero mayor"))
+
+while (a>=b):
+	b= input ("Intentalo de nuevo:")
+	print "Los numeros que has escrito" ,a,b 
+	
+	
+
