e4048e74463d85062f8da8a3052604b3a2fbc1e9
saniamunir <35691533+saniamunir@users.noreply.github.com>
2018-02-05 12:24:07 +0100

    Update bucles2.py

--- bucles2.py
+++ bucles2.py
@@ -1,18 +1,13 @@
 # coding:utf -8
 
-lim = float(input("Escribe el valor límite: "))
+lim = float(input("Escribe el valor: "))
 while lim <= 0:
-    lim = float(input("El lim debe ser mayor que 0. Inténtelo de nuevo: "))
-
+    lim = float(input("El valor debe ser mayor que 0. Inténtelo de nuevo: "))
 num = float(input("Escribe un número: "))
 suma = 0
 suma += num
-
 while suma < lim:
     num = float(input("Escribe otro número: "))
     suma += num
-
-print()
 print("Ha superado el lim. La suma de los num positivos es", str(suma) + ".")
-print("FIN")
 
